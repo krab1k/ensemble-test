0680d1a877d5aefce3eca9e16cf13335b48716c3
spirit01 <petra.hrozkova@gmail.com>
2017-09-06 00:00:48 +0200

    smazan output, upraven verbose, mazani tempdir, vse v docasne slozce, priprava na statistiku vysledku

--- src/ensemble.py
+++ src/ensemble.py
@@ -24,6 +24,24 @@ class Colors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+class RMSD_result:
+    def __init__(self, repeat = 0, selected_files = '', all_files = '', files_from_ensamble = '',
+                structure_and_rmsd = '', sum_rmsd = -100, files_and_weights = ''):
+                self.repeat = repeat
+                self.selected_files = selected_files
+                self.all_files = all_files
+                self.files_from_ensamble = files_from_ensamble
+                self.structure_and_rmsd = structure_and_rmsd
+                self.sum_rmsd = sum_rmsd
+                self.files_and_weights = files_and_weights
+
+    def print_result(self):
+        print('vysledek', self.repeat)
+
+
+
+
+
 
 def get_argument():
     parser = ArgumentParser()
@@ -49,6 +67,9 @@ def get_argument():
     parser.add_argument("--tolerance", type=float, dest="tolerance",
                         help="pessimist (0) or optimist (0 < x <1) result",
                         default=0)
+
+    parser.add_argument("--preserve", help="preserve temporary directory",
+                        action="store_true")
 
     return parser.parse_args()
 
@@ -76,31 +97,32 @@ def test_argument(n_files, k_options, list_pdb_file, tolerance):
         sys.exit(1)
 
 
-def print_parameters_verbose(n_files, k_options, list_pdb_file):
+def print_parameters_verbose(args, list_pdb_file, all_files, selected_files, files_and_weights):
     print(Colors.OKBLUE + 'Parameters \n' + Colors.ENDC)
     print('Working directory', os.getcwd(), '\n')
     print('Total number of available pdb files in the directory', len(list_pdb_file), '\n')
-    print('Number of the all used files', n_files, '\n')
-    print('Number of selected files', k_options, '\n')
-    print('All pdb.dat files \n')
-    for i in range(len(list_pdb_file)):
-        # print(repr(file).rjust(2))
+    print('Number of the all used files', args.n_files, '\n')
+    print('List of the all used files \n')
+    for i in range(len(all_files)):
         if (i + 1) % 7 == 0:
-            print(list_pdb_file[i])
+            print(all_files[i])
         else:
-            print(list_pdb_file[i], '\t', end='')
-
+            print(all_files[i], '\t', end='')
+    print('\n')
+    print('Number of selected files', args.k_options, '\n')
+    print('List of selected files \n')
+    for i in range(len(selected_files)):
+        if (i + 1) % 7 == 0:
+            print(selected_files[i])
+        else:
+            print(selected_files[i], '\t', end='')
     print('\n')
 
 
-def ensamble_fit(selected_files_for_ensamble, files_and_weights):
-    tmpdirname = tempfile.mkdtemp()
-
+def ensamble_fit(selected_files_for_ensamble, files_and_weights, tmpdirname):
     print(Colors.OKBLUE + '\nCreated temporary directory \n' + Colors.ENDC, tmpdirname, '\n')
-
     for i, f in enumerate(selected_files_for_ensamble, start=1):
-        shutil.copy(f, '{}/{:02d}.pdb'.format(tmpdirname, i))
-
+        shutil.copy(f, '{}/{:02d}.pdb.dat'.format(tmpdirname, i))
     make_curve_for_experiment(files_and_weights, tmpdirname)
     command = '/storage/brno3-cerit/home/krab1k/saxs-ensamble-fit/core/ensamble-fit -L -p {dir}/ -n {n} -m {dir}/curve.modified.dat'.format(
         dir=tmpdirname, n=len(selected_files_for_ensamble))
@@ -125,30 +147,28 @@ def work_with_result_from_ensamble(tmpdirname):
     return result_chi_and_weights
 
 
-def do_result(tolerance, all_files, selected_files, f, result_chi_and_weights):
+def do_result(tolerance, all_files, selected_files, result_chi_and_weights, tmpdirname):
     results = []
-
     minimum = min(result_chi_and_weights)[0]
     maximum = minimum * (1 + tolerance)
-
-    f.write('Interval chi2 :' + '\t' + 'minimum' + '\t' + str(minimum) + '\t' + 'maximum' + '\t' + str(maximum) + '\n')
     for chi2, weights in result_chi_and_weights:
         if chi2 <= maximum:
-            f.write('Value chi2: {}'.format(chi2))
-
             for i, weight in enumerate(weights):
                 if weight >= 0.001:
                     results.append((chi2, weight, all_files[i]))
 
             sum_rmsd = 0
             for k in results:
-                sum_rmsd += rmsd_pymol(selected_files[0], k[2], f) * float(k[1])
-            f.write('sum rmsd' + '\t' + str(sum_rmsd) + '\n')
+                sum_rmsd += rmsd_pymol(selected_files[0], k[2], tmpdirname) * float(k[1])
+
             print(Colors.OKBLUE + 'RMSD pymol' + Colors.ENDC, sum_rmsd, '\n')
 
 
 def make_curve_for_experiment(files_and_weights, tmpdirname):
     files = [filename for filename, weight in files_and_weights]
+    print(Colors.OKBLUE+'Data for ensamble fit \n'+ Colors.ENDC)
+    for data, weight in files_and_weights:
+        print('structure',data, 'weight', round(weight,3), '\n')
     qs = np.linspace(0, 0.5, 501)
     curves = {}
     for filename in files:
@@ -171,12 +191,13 @@ def make_curve_for_experiment(files_and_weights, tmpdirname):
 
     adderror("../data/exp.dat", tmpdirname + '/curve')
 
-
-def rmsd_pymol(structure_1, structure_2, f):
+def rmsd_pymol(structure_1, structure_2, tmpdirname):
+    shutil.copy(structure_1, tmpdirname)
+    shutil.copy(structure_2, tmpdirname)
     if structure_1 == structure_2:
         rmsd = 0
     else:
-        with open("file_for_pymol.pml", "w") as file_for_pymol:
+        with open(tmpdirname +'/file_for_pymol.pml', 'w') as file_for_pymol:
             file_for_pymol.write("""
             load  {s1}
             load  {s2}
@@ -186,45 +207,57 @@ def rmsd_pymol(structure_1, structure_2, f):
                        s3=os.path.splitext(structure_1)[0],
                        s4=os.path.splitext(structure_2)[0]))
         # out_pymol = subprocess.check_output("module add pymol-1.8.2.1-gcc; pymol -c file_for_pymol.pml | grep Executive:; module rm pymol-1.8.2.1-gcc", shell=True)
-        out_pymol = subprocess.check_output(" pymol -c file_for_pymol.pml | grep Executive:", shell=True)
+        command = ' pymol -c {dir}/file_for_pymol.pml | grep Executive:'.format(dir = tmpdirname)
+        out_pymol = subprocess.check_output(command, shell=True)
         rmsd = float(out_pymol[out_pymol.index(b'=') + 1:out_pymol.index(b'(') - 1])
-        f.write('RMSD ' + '\t' + structure_1 + ' and ' + structure_2 + ' = ' + str(rmsd) + '\n')
-
     print('RMSD ', structure_1, ' and ', structure_2, ' = ', rmsd)
+
+
     return rmsd
+
+def final_statistic(args):
+    print(Colors.HEADER + 'FINAL STATISTIC \n' + Colors.ENDC)
+    print('Number of repetitions', args.repeat, '\n')
+    print('THE BEST RMSD')
+    print('THE WORST RMSD')
 
 
 def main():
+    points = []
     args = get_argument()
     os.chdir(args.mydirvariable)
     list_pdb_file = find_pdb_file(args.mydirvariable)
     test_argument(args.n_files, args.k_options, list_pdb_file, args.tolerance)
 
-    if args.verbose:
-        print_parameters_verbose(args.n_files, args.k_options, list_pdb_file)
     for i in range(args.repeat):
+        c = RMSD_result()
+        tmpdirname = tempfile.mkdtemp()
         print(Colors.OKGREEN + 'RUN {}/{}'.format(i + 1, args.repeat) + Colors.ENDC, '\n')
-        filename = '/tmp/output_' + str(i) + '_' + str(args.n_files)
-        with open(filename, 'w') as f:
-            all_files = random.sample(list_pdb_file, args.n_files)
-            selected_files = random.sample(all_files, args.k_options)
-            f.write('All files for experiment' + '\n')
-            for filename in all_files:
-                f.write('{}\n'.format(filename))
+        all_files = random.sample(list_pdb_file, args.n_files)
+        selected_files = random.sample(all_files, args.k_options)
+        weights = np.random.dirichlet(np.ones(args.k_options), size=1)[0]
+        files_and_weights = list(zip(selected_files, weights))
+        c.selected_files = selected_files
+        c.all_files = all_files
+        c. files_and_weights = files_and_weights
+        c.repeat = i+1
+        points.append(c)
 
-            f.write('\n' + 'Selected files for experiment' + '\n')
-            for filename in selected_files:
-                f.write('{}\n'.format(filename))
+        if args.verbose:
+            print_parameters_verbose(args, list_pdb_file, all_files, selected_files, files_and_weights)
+        result_chi_and_weights = ensamble_fit(all_files, files_and_weights, tmpdirname)
 
-            weights = np.random.dirichlet(np.ones(args.k_options), size=1)[0]
-            files_and_weights = list(zip(selected_files, weights))
-            result_chi_and_weights = ensamble_fit(all_files, files_and_weights)
+        if args.k_options == 1:
+            do_result(args.tolerance, all_files, selected_files, result_chi_and_weights, tmpdirname)
+        else:
+            print(Colors.WARNING +'not implemented \n' + Colors.ENDC)
+        if not args.preserve:
+            shutil.rmtree(tmpdirname)
+    for point in points:
+        point.print_result()
+    final_statistic(args)
 
-            if args.k_options == 1:
-                do_result(args.tolerance, all_files, selected_files, f, result_chi_and_weights)
-            else:
-                print('not implemented')
-                sys.exit(1)
+
 
 
 if __name__ == '__main__':
